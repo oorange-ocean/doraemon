8eb6c5c4ba900474069d8e9112205a43a916c78d
A5anQHp <2910315591@qq.com>
2025-12-31 01:48:50 +0800

    feat: change character property

--- characters-data.js
+++ characters-data.js
@@ -4,7 +4,8 @@ const charactersData = [
         id: 'doraemon',
         icon: 'images/icon/doraemon_icon.png',
         cover: 'images/cover/doraemon.jpg',
-        brief: '来自22世纪的猫型育儿机器人，拥有四次元口袋，帮助大雄解决各种困难。',
+        brief: '来自22世纪的猫型育儿机器人，拥有四次元口袋。',
+        detail: '',
         photos: [],
         status: {
             height: '129.3cm',
@@ -19,7 +20,8 @@ const charactersData = [
         id: 'nobita',
         icon: 'images/icon/nobita_icon.png',
         cover: 'images/cover/nobita.jpg',
-        brief: '故事的主角，学习成绩差，运动能力弱，但心地善良，是哆啦A梦的好朋友。',
+        brief: '故事主角，成绩差但心地善良，是哆啦A梦的好朋友。',
+        detail: '',
         photos: [],
         status: {
             height: '140cm',
@@ -34,7 +36,8 @@ const charactersData = [
         id: 'shizuka',
         icon: 'images/icon/shizuka_icon.png',
         cover: 'images/cover/shizuka.jpg',
-        brief: '大雄的青梅竹马，温柔善良，学习成绩优秀，是大雄暗恋的对象。',
+        brief: '大雄的青梅竹马，温柔善良，是大雄暗恋的对象。',
+        detail: '',
         photos: [],
         status: {
             height: '136cm',
@@ -49,7 +52,8 @@ const charactersData = [
         id: 'suneo',
         icon: 'images/icon/suneo_icon.png',
         cover: 'images/cover/suneo.jpg',
-        brief: '家境富裕，喜欢炫耀，但有时也会帮助朋友，是大雄的同学之一。',
+        brief: '家境富裕，喜欢炫耀，但有时也会帮助朋友。',
+        detail: '',
         photos: [],
         status: {
             height: '136cm',
@@ -64,7 +68,8 @@ const charactersData = [
         id: 'takeshi',
         icon: 'images/icon/takeshi_icon.png',
         cover: 'images/cover/takeshi.jpg',
-        brief: '外号胖虎，力气大但脾气暴躁，喜欢唱歌但歌声难听，是大雄的同学。',
+        brief: '外号胖虎，力气大但脾气暴躁，喜欢唱歌。',
+        detail: '',
         photos: [],
         status: {
             height: '145cm',
@@ -79,7 +84,8 @@ const charactersData = [
         id: 'dekisugi',
         icon: 'images/icon/dekisugi_icon.png',
         cover: 'images/cover/dekisugi.jpg',
-        brief: '学习成绩优秀，各方面都很出色，是大雄的竞争对手，也是静香的好朋友。',
+        brief: '学习成绩优秀，各方面都很出色，是大雄的竞争对手。',
+        detail: '',
         photos: [],
         status: {
             height: '138cm',
@@ -94,7 +100,8 @@ const charactersData = [
         id: 'dorami',
         icon: 'images/icon/dorami_icon.png',
         cover: 'images/cover/dorami.jpg',
-        brief: '哆啦A梦的妹妹，比哥哥更优秀，拥有四次元口袋，经常帮助大雄和哆啦A梦。',
+        brief: '哆啦A梦的妹妹，比哥哥更优秀，拥有四次元口袋。',
+        detail: '',
         photos: [],
         status: {
             height: '100cm',
@@ -109,7 +116,8 @@ const charactersData = [
         id: 'jaiko',
         icon: 'images/icon/jaiko_icon.png',
         cover: 'images/cover/jko.jpg',
-        brief: '胖虎的妹妹，喜欢画漫画，梦想成为漫画家，性格温和。',
+        brief: '胖虎的妹妹，喜欢画漫画，梦想成为漫画家。',
+        detail: '',
         photos: [],
         status: {
             height: '135cm',
@@ -124,7 +132,8 @@ const charactersData = [
         id: 'nobisuke',
         icon: 'images/icon/nobisuke_icon.png',
         cover: 'images/cover/nobisuke.jpg',
-        brief: '大雄的父亲，在公司上班，年轻时也有过梦想，对家人很关心。',
+        brief: '大雄的父亲，在公司上班，对家人很关心。',
+        detail: '',
         photos: [],
         status: {
             height: '170cm',
@@ -139,7 +148,8 @@ const charactersData = [
         id: 'tamako',
         icon: 'images/icon/tamako_icon.png',
         cover: 'images/cover/tamako.jpg',
-        brief: '大雄的母亲，家庭主妇，对儿子的学习很关心，有时会责备大雄。',
+        brief: '大雄的母亲，家庭主妇，对儿子的学习很关心。',
+        detail: '',
         photos: [],
         status: {
             height: '158cm',
@@ -154,7 +164,8 @@ const charactersData = [
         id: 'sewashi',
         icon: 'images/icon/sewashi_icon.png',
         cover: 'images/cover/sewashi.jpg',
-        brief: '大雄的玄孙，来自22世纪，是哆啦A梦的原主人，派哆啦A梦来帮助大雄。',
+        brief: '大雄的玄孙，来自22世纪，是哆啦A梦的原主人。',
+        detail: '',
         photos: [],
         status: {
             height: '120cm',
